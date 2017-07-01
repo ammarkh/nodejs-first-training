863ce97f34a8f3050ac0b377a7de30065c4b9cc4
ammarkh <a.m.k-it@hotmail.com>
2017-07-01 04:06:16 +0300

    strated code

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var sql = require('./Modules/SqlQuery');
+// create the instance from object sql pass a database information
+
+var Query = new sql({host: "localhost", userName: "", password: "", database: "schooldb"});
+var ListEmployee = Query.selectQuery('select * from Students', function (err, resault) {
+    if (err) throw err;
+    else
+        return resault;
+});
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+
+    Query.selectQuery('select * from Students', function (err, resault) {
+        if (err) throw err;
+        else
+            res.render('index',{
+               List : resault
+            });
+    });
+});
+
+
+module.exports = router;
